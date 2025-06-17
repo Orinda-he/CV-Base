e846c7452677b120a204df1961395395e3acd829
ya.he <ya.he@iluvatar.com>
2025-06-17 22:57:34 +0800

    add

--- digit_recognition_2/train_neuralnet.py
+++ digit_recognition_2/train_neuralnet.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
+sys.path.append('/Users/heya/Desktop/CV-Base')  # 为了导入父目录的文件而进行的设定
 import numpy as np
 import matplotlib.pyplot as plt
-from dataset.mnist import load_mnist
+from digit_recognition.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 
 # 读入数据
